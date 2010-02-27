54f5ad6090e32dfab47303b5f0a866904ad2935b
juanma.cabello@me.com <juanma.cabello.b@gmail.com>
2010-02-27 09:16:57 +0100

    CSS subtraction and more documentation on the methods.

--- class.Tipz.js
+++ class.Tipz.js
@@ -1,8 +1,8 @@
 /*
   Name: Tipz
   Author: Juanma Cabello
-  Date last modification: 25/02/2010
-  Version: 0.1
+  Last modification date: 25/02/2010
+  Version: 0.2
   
   Show a fancy tooltip from a DOMObject passed as parameter.
 */
@@ -28,57 +28,59 @@ function Tipz(params) {
 // Methods section
 /* 
   @name        show 
-  @description Prints the tooltip on screen
+  @description Prints the tooltip on screen. 
+               
+               All the tooltip is inside the canvas div. 
+               There are two parts: the arrow and the content. 
+               
+               - The arrow is formed with 5 1 pixel height divs in stack. 
+                 Each one has 2 more pixels than the previous starting from 1 pixel, so
+                 they form a little pyramid pointing to the top.
+               
+               - The content is the tip itself.
   
-  @params      DOMObject
+  @params      DOMObject The DOM object which the tooltip is for.
+  @params      tip       The text which is going to be shown in the tooltip.
+  
   @return      true
 */
 Tipz.prototype.show = function(DOMObject, tip) {
   // Canvas where the tooltip is going to be printed
   var canvas = document.createElement('div');
   canvas.id = this.id;
-  canvas.style.margin = '2px';
-  canvas.style.position = 'absolute';
-  canvas.style.fontFamily = 'helvetica';
-  canvas.style.fontSize = '10px';
-  canvas.style.fontWeight = 'bold';
+  canvas.className = 'canvas';
   
   // Arrow printing
   var arrow = document.createElement('div');
-  arrow.style.textAlign = 'center';
+  arrow.className = 'arrow';
     // First row 
   var firstRow = document.createElement('div');
+  firstRow.className = 'row';
   firstRow.style.width = '1px';
-  firstRow.style.height = '1px';
-  firstRow.style.margin = '0 auto 0 auto';
   firstRow.style.backgroundColor = this.backgroundColor;
   arrow.appendChild(firstRow);
     // Second row
   var secondRow = document.createElement('div');
+  secondRow.className = 'row';
   secondRow.style.width = '3px';
-  secondRow.style.height = '1px';
-  secondRow.style.margin = '0 auto 0 auto';
   secondRow.style.backgroundColor = this.backgroundColor;
   arrow.appendChild(secondRow);
     // Third row
   var thirdRow = document.createElement('div');
+  thirdRow.className = 'row';
   thirdRow.style.width = '5px';
-  thirdRow.style.height = '1px';
-  thirdRow.style.margin = '0 auto 0 auto';
   thirdRow.style.backgroundColor = this.backgroundColor;
   arrow.appendChild(thirdRow);
     // Fourth row
   var fourthRow = document.createElement('div');
+  fourthRow.className = 'row';
   fourthRow.style.width = '7px';
-  fourthRow.style.height = '1px';
-  fourthRow.style.margin = '0 auto 0 auto';
   fourthRow.style.backgroundColor = this.backgroundColor;
   arrow.appendChild(fourthRow);
     // Fifth row
   var fifthRow = document.createElement('div');
+  fifthRow.className = 'row';
   fifthRow.style.width = '9px';
-  fifthRow.style.height = '1px';
-  fifthRow.style.margin = '0 auto 0 auto';
   fifthRow.style.backgroundColor = this.backgroundColor;
   arrow.appendChild(fifthRow);
   
@@ -87,12 +89,7 @@ Tipz.prototype.show = function(DOMObject, tip) {
   
   // The tip container
   var content = document.createElement('div');
-  content.style.padding = '5px';
-  content.style.borderRadius = '2px'; 
-  
-  if (this.hasShadow) {
-    content.style.boxShadow = '10px 10px 5px #888';
-  }
+  content.className = 'content';
   
   content.style.backgroundColor = this.backgroundColor;
   content.style.color = this.color;
@@ -119,7 +116,7 @@ Tipz.prototype.show = function(DOMObject, tip) {
 
 /* 
   @name        hide 
-  @description Delete the tooltip on screen
+  @description Simply, delete the tooltip on screen
   
   @return      true
 */
